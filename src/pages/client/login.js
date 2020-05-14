e927ebc298ea96e38f5bc3a47122e597ba2cf73a
ydatech <yudasukmana1992@gmail.com>
2020-05-14 20:23:37 +0700

    remove unnecessary import

--- src/pages/client/login.js
+++ src/pages/client/login.js
@@ -2,12 +2,6 @@ import React, { useState } from 'react';
 
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
